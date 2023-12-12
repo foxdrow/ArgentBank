db4b0460eff463dd9ed8cf0ccd604b76548d64b2
Raphael <rapha.saverys@gmail.com>
2023-12-12 16:04:21 +0100

    create Home Page

---
+++ client/src/pages/SignIn/SignIn.jsx
@@ -0,0 +1,10 @@
+import MainContainerLayout from "../../layouts/MainContainerLayout/MainContainerLayout";
+import "./SignIn.scss";
+
+export const SignIn = () => {
+  return (
+    <MainContainerLayout className="sign-in">
+
+    </MainContainerLayout>
+  )
+}
