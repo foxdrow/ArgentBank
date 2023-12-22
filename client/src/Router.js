293d7932879425907426f1cc614789063a16cb1b
Raphael <rapha.saverys@gmail.com>
2023-12-22 11:00:43 +0100

    add the user page

--- client/src/Router.js
+++ client/src/Router.js
@@ -1,6 +1,7 @@
 import { Routes, Route } from "react-router-dom";
 import Home from "./pages/Home/Home";
 import SignIn from "./pages/SignIn/SignIn";
+import User from "./pages/User/User";
 
 
 const Router = () => {
@@ -8,6 +9,7 @@ const Router = () => {
         <Routes>
             <Route path="/" element={<Home />} />
             <Route path="/sign-in" element={<SignIn />} />
+            <Route path="/user" element={<User />} />
         </Routes>
     )
 }
